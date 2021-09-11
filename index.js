72409273b39e46773a218f09530b3f83f8b4ebb5
Abhijeet Verma <vermaabhijeet29@gmail.com>
2021-09-11 23:55:09 +0530

    minor fix for dependency issue resolve

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const emailVal = require('deep-email-validator')
 const http = require('http')
 const path = require('path')
 const bodyParser = require('body-parser')
+var axios = require("axios").default;
 var app = express();
+require('dotenv').config()
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,7 +16,7 @@ http.createServer(app).listen(3001, () =>{
 });
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname+'/views/email-validator-form.html'))
+  res.sendFile(path.join(__dirname+'/views/email-validator-form.html'))
 })
 
 app.post('/validate-email', async function(req, res) {
@@ -27,16 +28,34 @@ app.post('/validate-email', async function(req, res) {
     })
   }
  
-  const {valid, reason, validators} = await isEmailValid(email);
- 
-  if (valid) return res.send({success: true, msg: 'Email Id is correct.'});
- 
-  return res.status(200).send({
-  	success: false,
-    msg: validators[reason].reason,
-  })
+  let isValid = false;
+  checkEmail(email).then(resp => {
+    isValid = resp.data.valid;
+    if (isValid) return res.send({success: true, msg: 'Email Id is correct.'});
+    else{
+      return res.status(200).send({
+        success: false,
+        msg: 'Email id is not correct.',
+      })
+    }
+
+  }).catch(function (error) {
+    return res.status(200).send({
+      success: false,
+      msg: 'Email id is not correct.',
+    })
+  });
 })
 
 async function isEmailValid(email) {
  return emailVal.validate(email)
+}
+
+function checkEmail(email){
+  let isValid = false;
+  var options = {
+    method: 'GET',
+    url: 'https://ipqualityscore.com/api/json/email/'+process.env.ipQualityScoreAPIKey+'/'+email,
+  };
+  return axios.request(options)
 }
